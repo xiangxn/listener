1737071b83a7240cf94947bb01973ee30122006a
necklace <xiangxn@163.com>
2024-08-07 20:12:04 +0800

    update download config sh, and sync configs

--- download_config.sh
+++ download_config.sh
@@ -9,7 +9,7 @@ echo -n "IP: "
 read ip
 read -s -p "Password: " pw
 echo
-sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/config.json ./${NET_NAME}.config.yaml
-sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/token_blacklist.json ./${NET_NAME}_token_blacklist.json
-sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/pool_blacklist.json ./${NET_NAME}_pool_blacklist.json
-sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/token_erc20a.json ./${NET_NAME}_token_erc20a.json
+sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/${NET_NAME}.config.yaml ./${NET_NAME}.config.yaml
+sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/${NET_NAME}_token_blacklist.json ./${NET_NAME}_token_blacklist.json
+sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/${NET_NAME}_pool_blacklist.json ./${NET_NAME}_pool_blacklist.json
+sshpass -p $pw rsync -avz --progress root@$ip:/root/listener/${NET_NAME}_token_erc20a.json ./${NET_NAME}_token_erc20a.json
